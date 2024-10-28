7ed39ae9f93733d0a30d95884b35f3dfd4b80d58
Admin <Admin@172.25.24.198>
2024-10-28 16:57:13 +0700

    first comit

---
+++ assignment/src/assignment/Assignment.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package assignment;
+
+/**
+ *
+ * @author Admin
+ */
+public class Assignment {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+       
+      System.out.println("Ho va Ten:Do van Hieu");
+      System.out.println("MaSV:29211153547");
+    }
+  }
+ 
+    
+
